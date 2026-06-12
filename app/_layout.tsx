d420a59af2647425a2b40c74944154757bdcdea2
rohit-2604 <rohitneogi26042002@gmail.com>
2026-06-12 11:27:59 +0530

    initializing first app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,52 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
+import "../global.css"
+import {
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const properties = [
+  {
+    id: "1",
+    title: "Modern Villa",
+    city: "Mumbai",
+    price: "₹1.2Cr",
+  },
+  {
+    id: "2",
+    title: "Sea View Flat",
+    city: "Mumbai",
+    price: "₹85L",
+  },
+  {
+    id: "3",
+    title: "Studio Loft",
+    city: "Bangalore",
+    price: "₹32L",
+  },
+];
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaView className="bg-white p-10 flex-1">
+      <View>
+        {/* <Text>Edit app/index.tsx to edit this screen.</Text> */}
+        <TextInput placeholder="Search City..." />
+        <TouchableOpacity onPress={() => alert("Searching!")}>
+          <Text>Search</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList data={properties} keyExtractor={(item)=>item.id} 
+        renderItem={({item})=>(
+          <View>
+            <Text >{item.title}</Text>
+            <Text >{item.city}</Text>
+            <Text >{item.price}</Text>
+          </View>
+        )}/>
+    </SafeAreaView>
   );
 }
