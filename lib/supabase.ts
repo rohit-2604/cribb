574e387ab30a8e94a4e770c729d7a73a0098e9eb
rohit-2604 <rohitneogi26042002@gmail.com>
2026-06-19 18:52:53 +0530

    updated backend and supabase !

---
+++ lib/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY as string
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export function createClerkClient(getToken:()=>Promise<string>) {
+    
+}
